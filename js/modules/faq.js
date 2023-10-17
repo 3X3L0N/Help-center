4f05fd726170f910451f1f6cf01bf559964fc03f
3X3L0N <josuejaureguicamacho@gmail.com>
2023-10-17 14:43:22 -0500

    basic control center finished

---
+++ js/modules/faq.js
@@ -0,0 +1,29 @@
+const FAQ = () => {
+  const categories = document.getElementById("categories");
+
+  const fetchData = async () => {
+    const response = await fetch("./json/database.json");
+    const data = await response.json();
+
+    data.forEach((element) => {
+      const { id, image, title, slug, text } = element;
+      categories.innerHTML += `
+          <div class="card card--flip" key="${id}">
+            <div class="card__body card__body--flip">
+              <img
+                src="${image}"
+                width="48px" height="48px" class="img img--category"/>
+              <h2 class="card__title">${title}</h2>
+            </div>
+            <div class="card__body card__body--flip">
+              <p class="card__text">${text}</p>
+              <a href="${slug}" class="button button--primary">Ver preguntas</a>
+            </div>
+          </div>
+        `;
+    });
+  };
+  fetchData();
+};
+
+export default FAQ;
